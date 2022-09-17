0e428764ea4a302b92226aa9fc74c4ac8b5d278a
don <misurakalu@gmail.com>
2022-09-17 17:46:32 +1000

    Basic Site working with diegos help

--- wikipedia-client-server-example/routes/index.js
+++ wikipedia-client-server-example/routes/index.js
@@ -1,26 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+// const yelp = require('yelp-fusion');
+
+const apiKey = 'kR1ROdAbsem5zPOGEUgkl1M94Lm0SkDkUtNucQgIbpgB70FceTLSEHoztwhjiFZFA20RYrDl74Ypam4LbgOV1AET0MvLdDtWLFLn56d63sUMxw3tCnrN1MJEmpsVY3Yx';
+
 
 /* GET home page. */
-router.get("/", function (req, res) {
-  const WIKI_ENDPOINT =
-    "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/2020/08/27";
+router.get("/", function(req, res) {
+    const YELP_ENDPOINT =
+        //"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/2020/08/27";
+        // yelp api key 
+        "https://api.yelp.com/v3/businesses/search?term=chicken&latitude=-27.470125&longitude=153.021072";
 
-  axios
-    .get(WIKI_ENDPOINT)
-    .then((response) => {
-      const { data } = response;
-      const allArticles = data.items[0].articles;
+    axios
+        .get(YELP_ENDPOINT, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`
+            }
+        })
+        .then((response) => {
+            // const { data } = response;
+            console.log(response.data);
+            // const allArticles = data.items[0].articles;
+            const allShops = response.data.businesses;
+            //console.log(allShops);
 
-      // We only want the top 10 articles
-      // Let's also remove the first two articles as they are always the Home & Search pages
-      const topTenArticles = allArticles.splice(2, 10);
-      res.render("index", { articles: topTenArticles });
-    })
-    .catch((error) => {
-      console.log(error.toJSON());
-    });
+
+            // We only want the top 10 articles
+            // Let's also remove the first two articles as they are always the Home & Search pages
+            res.render("index", { shops: allShops });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 module.exports = router;
