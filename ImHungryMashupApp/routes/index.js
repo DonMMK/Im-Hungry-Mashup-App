13a48d76a316ec27a0c8e8c762f9c2f07456ab0c
don <misurakalu@gmail.com>
2022-09-20 09:11:32 +1000

    search bar updated

--- ImHungryMashupApp/routes/index.js
+++ ImHungryMashupApp/routes/index.js
@@ -4,15 +4,20 @@ const axios = require("axios");
 const yelp = require('yelp-fusion');
 
 const apiKey = 'kR1ROdAbsem5zPOGEUgkl1M94Lm0SkDkUtNucQgIbpgB70FceTLSEHoztwhjiFZFA20RYrDl74Ypam4LbgOV1AET0MvLdDtWLFLn56d63sUMxw3tCnrN1MJEmpsVY3Yx';
-
+const shopTerm = "korean";
+const shopLat = "-27.470125";
+const shopLong = "153.021072";
 
 /* GET home page. */
 router.get("/", function(req, res) {
+    console.log("before yelp api endpoint");
     const YELP_ENDPOINT =
+        // default wiki endpoint for testing
         //"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/2020/08/27";
         // yelp api key 
-        "https://api.yelp.com/v3/businesses/search?term=chicken&latitude=-27.470125&longitude=153.021072";
+        `https://api.yelp.com/v3/businesses/search?term=${shopTerm}&latitude=${shopLat}&longitude=${shopLong}`;
 
+    console.log("after yelp api endpoint");
     axios
         .get(YELP_ENDPOINT, {
             headers: {
@@ -21,7 +26,7 @@ router.get("/", function(req, res) {
         })
         .then((response) => {
             // const { data } = response;
-            console.log(response.data);
+            // console.log(response.data);
             // const allArticles = data.items[0].articles;
             const allShops = response.data.businesses;
             //console.log(allShops);
@@ -34,6 +39,14 @@ router.get("/", function(req, res) {
         .catch((error) => {
             console.log(error);
         });
+});
+
+router.post("/", function(req, res) {
+    // res.send(JSON.stringify(req.body));
+    // res.redirect('/');
+    const shopInputName = req.body.search;
+    console.log(shopInputName);
+    console.log("inside index.js");
 });
 
 module.exports = router;
